a37111a4e4a342bb480ccc41fa58c388eca9692c
Sherina Eria Hastalina <hastalinas@gmail.com>
2023-07-19 15:05:51 +0700

    Initial commit

---
+++ CRUD/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CRUD.Models;
+namespace CRUD.Data;
+
+public class ContactsAPIDbContext : DbContext
+{
+    public ContactsAPIDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    public DbSet<Contact> Contacts { get; set; }
+}
---
+++ CRUD/Models/AddContactRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace CRUD.Models;
+
+public class AddContactRequest
+{
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public long Phone { get; set; }
+    public string Address { get; set; }
+}
